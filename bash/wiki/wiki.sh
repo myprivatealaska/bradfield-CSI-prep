8530f6a36df977041ece0f57c66d99b3553a6500
myprivatealaska <natallia.perats@talentinc.com>
2021-02-28 11:11:39 -0500

    Update subsections logic

--- bash/wiki/wiki.sh
+++ bash/wiki/wiki.sh
@@ -12,8 +12,8 @@ if [ "$#" = "0" ]; then
   echo -e "Must provide article name, e.g. ./wiki.sh walrus"
   exit 1
 fi
-article=$(echo "$1" | awk '{for (i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1')
-subsection=$2
+article=$(echo "$1" | awk '{for (i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1' | tr ' ' '_')
+subsection=$(echo "$2" | awk '{for (i=1;i<=1;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1' | tr ' ' '_')
 
 echo -e "Article name: $article";
 echo -e "Subsection: $subsection";
@@ -32,8 +32,16 @@ fi
 
 echo $use_local
 
-if [ "$subsection" = "" ]; then
+article_html=$(if [ "$use_local" ]; then cat "$data_file"; else curl -s "$WIKI_URL$article"; fi)
+
+if [[ "$subsection" = "" ]]
+then
   # First legit sentence of the article lies within a div element with a "mw-parser-output" class and is usually inside of the first non-empty <p> tag
-  (if [ "$use_local" ]; then cat "$data_file"; else curl -s "$WIKI_URL$article"; fi) | grep -A 1000 '<div class="mw-parser-output"' | grep -i "$article" | grep "<p>" | head -n 1 | cut -d "." -f1
+  echo "$article_html" | grep -A 1000 '<div class="mw-parser-output"' | grep -i "$article" | grep "<p>" | head -n 1 | cut -d "." -f1 | sed -e 's/<[^>]*>//g'
+
+  echo "$article_html" | grep "toclevel-1" | sed -e 's/<[^>]*>//g'
+else
+  echo "$article_html" | grep -A 5 "id=\"$subsection"\" | grep "<p>" | head -n 1 | cut -d "." -f1 | sed -e 's/<[^>]*>//g'
+
 fi
 
